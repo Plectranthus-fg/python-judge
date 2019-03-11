ad1d6e8635a5ff5232326d29f83a0db7235f2e78
Plectranthus_fg <z1304561248@outlook.com>
2019-03-12 06:42:02 +0900

    python judging with one thread

--- judge.cpp
+++ judge.cpp
@@ -1,8 +1,31 @@
-#include "python/Python.h"
+#include <python3.6m/Python.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+#define filename "test.py"
 
 int main(int argc, char const *argv[]) {
-  Py_Initalize();
-  PyRun_SimpleString("print('Hello word')\n");
+  char resultname[] = "/tmp/fileXXXXXX";
+  auto fd = mkstemp(resultname);
+  freopen(resultname, "a", stdout);
+
+  auto pyfile = fopen(filename, "r");
+  if (!pyfile) {
+    std::cerr << "Fail to open " << filename <<std::endl;
+  }
+  Py_Initialize();
+  PyRun_SimpleFile(pyfile, filename);
   Py_Finalize();
+  fclose(stdout);
+  std::string resultstring;
+  char buf;
+
+  auto result = fopen(resultname,"r");
+  while ((buf = fgetc(result)) != EOF)
+    resultstring.push_back(buf);
+  std::cerr << resultstring <<std::endl;
   return 0;
 }
